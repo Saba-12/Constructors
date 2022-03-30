cc257140c3251c5b63df3277c7c0d9092baab370
Saba-12 <101691122+Saba-12@users.noreply.github.com>
2022-03-30 12:01:23 +0530

    parameterized constructor

---
+++ Constructore-YT/ConsoleApp1/ParameterizedConstructor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterizedConstructor
+{
+    internal class Program
+    {
+        int x;
+        public Program(int i)
+        {
+            x = i;
+            Console.WriteLine("Paramterized constructor is called with value = "+i);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Value of x = " + x);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program(10);
+            Program p1 = new Program(20);
+
+            p.Display();
+            p1.Display();
+            Console.ReadLine();
+        }
+    }
+}
