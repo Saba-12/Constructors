86da97032d2e905328fa08765f71ed70ac3e384e
Saba-12 <101691122+Saba-12@users.noreply.github.com>
2022-03-30 09:50:18 +0530

    commit

---
+++ Constructore-YT/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private int i;
+        private bool b;
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine(program.i);
+            Console.WriteLine(program.b);
+
+            Console.ReadLine();
+        }
+}
+}
---
+++ Constructore-YT/ConsoleApp1/ExplicitConstructorDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitConstructorDemo
+{
+    internal class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("Constructor is called");
+        }
+        
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Program program1 = new Program();
+            Program program2 = new Program();
+            Console.ReadLine();
+        }
+    }
+}
