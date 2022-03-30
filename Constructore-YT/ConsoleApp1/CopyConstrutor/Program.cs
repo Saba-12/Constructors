ab04a121dc8a28617359bd5a22c90dfdb54eac58
Saba-12 <101691122+Saba-12@users.noreply.github.com>
2022-03-30 12:17:12 +0530

    copy constructor

---
+++ Constructore-YT/ConsoleApp1/CopyConstrutor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstrutor
+{
+    internal class Program
+    {
+        int x;
+
+        public Program(int i)
+        {
+            x = i;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Value of x = "+x);
+        }
+
+        public Program(Program obj)
+        {
+            x = obj.x;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program(10);
+            p.Display();
+
+            Program p1 = new Program(p);
+            p1.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
