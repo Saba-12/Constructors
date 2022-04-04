ed38069ca804ad4c8155846ead756789acda19c2
Saba-12 <101691122+Saba-12@users.noreply.github.com>
2022-04-04 10:17:25 +0530

    commit

---
+++ Constructore-YT/ConsoleApp1/StaticVsNonStaticConstructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVsNonStaticConstructor
+{
+    internal class Program
+    {
+        static int y;
+        int x;
+        static Program()
+        {
+            Console.WriteLine("Static Constructor is called");
+        }
+
+        public Program()
+        {
+            Console.WriteLine("Non-Static Constructor is called");
+        }
+        static void Main(string[] args)
+        {
+            Program program=new Program();
+            Console.WriteLine("Main Method is called");
+            Console.ReadLine();
+        }
+    }
+}
